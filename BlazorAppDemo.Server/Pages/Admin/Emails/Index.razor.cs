b20afccf5fbcce99eab1edb1bbdfa04eef216f1e
matjmiles <milesm@byui.edu>
2023-04-10 14:22:51 -0600

    index.razor of Emails

---
+++ BlazorAppDemo.Server/Pages/Admin/Emails/Index.razor.cs
@@ -0,0 +1,44 @@
+﻿using BlazorAppDemo.Core.Interfaces;
+using BlazorAppDemo.Shared.Print3dModels;
+using Library.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorAppDemo.Server.Pages.Admin.Emails;
+
+public partial class Index
+{
+
+    [Inject] private IPrint3dRepository DataService { get; set; }
+    public List<EmailModel> AllEmails { get; set; }
+    public string SaveError { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            AllEmails = await DataService.GetEmails();
+        }
+        catch (Exception ex)
+        {
+            SaveError = $"Error retreiving Emails{ex.Message}";
+
+        }
+    }
+
+    protected async Task CreateEmailAsync()
+    {
+    }
+
+    protected async Task UpdateEmailAsync(int EmailId)
+    {
+    }
+
+    protected async Task DeleteEmailAsync(int EmailId)
+    {
+    }
+
+
+
+
+ }
