06c6341bc173eaa0b9f436087a9a1b88614fe16b
matjmiles <milesm@byui.edu>
2023-04-13 13:38:31 -0600

    Replaced == with ** in Markdown Files. == not supported on Github

--- BlazorAppDemo.Shared/Print3dModels/StatusModel.cs
+++ BlazorAppDemo.Shared/Print3dModels/StatusModel.cs
@@ -4,28 +4,18 @@ public partial class StatusModel
 {
 
     public int StatusId { get; set; }
-
     public string Name { get; set; }
-
     public bool AcceptPayment { get; set; }
-
     public bool InQueue { get; set; }
-
     public bool PatronCanDelete { get; set; }
-
     public bool DisplayOnDashboard { get; set; }
-
     public bool SubtractInventory { get; set; }
-
     public bool Completed { get; set; }
-
     public int? Order { get; set; }
-
     public DateTime UpdatedAt { get; set; }
 
     // foreign key relationship
     public int? EmailId { get; set; }
-
     public virtual EmailModel Email { get; set; }
 
 }
