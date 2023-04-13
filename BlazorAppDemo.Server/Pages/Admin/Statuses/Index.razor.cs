e8558723c43707830e204631d0cdc71ff6bf1b46
matjmiles <milesm@byui.edu>
2023-04-13 16:48:35 -0600

    FK Crud operations docs

---
+++ BlazorAppDemo.Server/Pages/Admin/Statuses/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using BlazorAppDemo.Core.Interfaces;
+using BlazorAppDemo.Shared.Print3dModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorAppDemo.Server.Pages.Admin.Statuses;
+
+public class Statuses
+{
+
+    [Inject] private IPrint3dRepository DataService { get; set; }
+
+    public List<StatusModel> AllStatuses { get; set; }
+
+    public string SaveError { get; set; }
+}
+
+
