81dd6f01a1b051bffd2a8625c834fb6ecf4840d6
matjmiles <milesm@byui.edu>
2023-04-14 12:33:59 -0600

    FK table CRUD docs

---
+++ BlazorAppDemo.Server/Pages/Admin/Statuses/AddOrUpdateStatusDialog.razor.cs
@@ -0,0 +1,94 @@
+﻿using BlazorAppDemo.Core.Interfaces;
+using BlazorAppDemo.Shared.Print3dModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlazorAppDemo.Server.Pages.Admin.Statuses;
+
+public partial class AddOrUpdateStatusDialog
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+    [Parameter] public StatusModel statusModel { get; set; } = new StatusModel();
+
+    [Parameter] public int? StatusId { get; set; }
+
+    [Parameter] public int? EmailId { get; set; }
+
+    public List<EmailModel> AllEmails { get; set; }
+
+    public int? SelectedEmailId { get; set; }
+
+    public string SaveError { get; set; }
+
+
+    [Inject] private IPrint3dRepository DataService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (statusModel.EmailId == null)
+        {
+            SelectedEmailId = 0;
+        }
+        else
+        {
+            SelectedEmailId = statusModel.EmailId;
+        }
+
+        AllEmails = await DataService.GetEmailsAsync();
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+
+    protected async Task Submit()
+    {
+        var dialog = DialogResult.Ok(statusModel);
+        StatusModel statusMod = dialog.Data as StatusModel;
+
+        if (statusModel.StatusId < 0)
+        {
+            try
+            {
+                await DataService.CreateStatusAsync(statusMod);
+                MudDialog.Close();
+            }
+
+            catch (Exception ex)
+            {
+                SaveError = $"Error when saving Status:{ex.Message}";
+                // Logger.LogError(ex, "Error when trying to save a purchase request");
+            }
+
+        }
+        else
+        {
+            try
+            {
+                await DataService.UpdateStatusAsync(statusMod);
+                MudDialog.Close();
+            }
+
+            catch (Exception ex)
+            {
+                SaveError = $"Error when saving Status:{ex.Message}";
+                // Logger.LogError(ex, "Error when trying to save a purchase request");
+            }
+
+        }
+
+    }
+
+    public void ChangeEmailId(int? selected)
+    {
+        SelectedEmailId = selected;
+        statusModel.EmailId = selected;
+
+        this.StateHasChanged();
+    }
+
+
+
+}
--- BlazorAppDemo.Server/Pages/Admin/Statuses/Index.razor.cs
+++ BlazorAppDemo.Server/Pages/Admin/Statuses/Index.razor.cs
@@ -5,14 +5,80 @@ using MudBlazor;
 
 namespace BlazorAppDemo.Server.Pages.Admin.Statuses;
 
-public class Statuses
+public partial class Index
 {
 
     [Inject] private IPrint3dRepository DataService { get; set; }
-
     public List<StatusModel> AllStatuses { get; set; }
-
     public string SaveError { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            AllStatuses = await DataService.GetStatusesAsync();
+        }
+        catch (Exception ex)
+        {
+            SaveError = $"Error retreiving Statuses{ex.Message}";
+
+        }
+    }
+
+    protected async Task CreateStatusAsync()
+    {
+        var parameters = new DialogParameters();
+        parameters.Add("statusModel", new StatusModel());
+        var dialog = await _dialogService.Show<AddOrUpdateStatusDialog>("Create A New Status Entry", parameters).Result;
+
+        try
+        {
+            AllStatuses = await DataService.GetStatusesAsync();
+        }
+        catch (Exception ex)
+        {
+            SaveError = $"Error retreiving Statuses{ex.Message}";
+        }
+    }
+
+    protected async Task UpdateStatusAsync(int statusId)
+    {
+        var parameters = new DialogParameters();
+        var statusNeedToUpdate = AllStatuses.FirstOrDefault(_ => _.StatusId == statusId);
+        parameters.Add("statusModel", statusNeedToUpdate);
+        var dialog = await _dialogService.Show<AddOrUpdateStatusDialog>("Update A Status", parameters).Result;
+
+        try
+        {
+            AllStatuses = await DataService.GetStatusesAsync();
+        }
+        catch (Exception ex)
+        {
+            SaveError = $"Error retreiving Statuses{ex.Message}";
+        }
+    }
+
+    protected async Task DeleteStatusAsync(int statusId)
+    {
+        bool? result = await _dialogService.ShowMessageBox(
+        "Delete Confirmation",
+        "Deleting can not be undone!",
+        yesText: "Delete!", cancelText: "Cancel");
+
+        if (result ?? false)
+        {
+            try
+            {
+                await DataService.DeleteStatusAsync(statusId);
+                AllStatuses = await DataService.GetStatusesAsync();
+            }
+            catch (Exception ex)
+            {
+                SaveError = "You cannot delete this Status. It is attached to a job";
+            }
+        }
+    }
+
 }
 
 
